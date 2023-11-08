fc47813f4cceed0587350d3ba85fd3dfc4779358
bestwestern <runjep@gmail.com>
2023-11-08 21:29:50 +0100

    supabase

--- src/components/Counter.svelte
+++ src/components/Counter.svelte
@@ -2,7 +2,22 @@
 	import { quintOut } from 'svelte/easing';
 	import { crossfade } from 'svelte/transition';
 	import { flip } from 'svelte/animate';
-
+	import { createClient } from "@supabase/supabase-js";
+	const supabaseUrl = "https://upabdmzybbgsnbonhgmc.supabase.co";
+  const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVwYWJkbXp5YmJnc25ib25oZ21jIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODExMjg1NTMsImV4cCI6MTk5NjcwNDU1M30.5TeDInNIPfxKeE_KG4GcUEznh4i9wbKjUek935JSq6Y";
+  const supabase = createClient(
+	"https://upabdmzybbgsnbonhgmc.supabase.co",
+	supabaseKey
+	);
+	onMount(async () => {
+		let { data: products, error } = await supabase
+		.from("products")
+		.select("*")
+		//.eq("id", productid);
+	});
+	''
+  console.log(products);
 	const [send, receive] = crossfade({
 		fallback(node, params) {
 			const style = getComputedStyle(node);
